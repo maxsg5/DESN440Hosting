25fe1652d611dca3e6177c357885fb2573684c91
Max Schafer <schaferm5@mymacewan.ca>
2024-02-08 18:27:32 -0700

    P1

--- desn440/P1/script.js
+++ desn440/P1/script.js
@@ -1,6 +1,8 @@
+//enabling tool tips.
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+//mouse event for the circle following the mouse in the Hero section.
 document.addEventListener('mousemove', function (e) {
     const mask = document.getElementById('mask');
     const hero = document.getElementById('Hero');
